e53d0dd9aa375bb3e975226e8136b8be1868c9da
Quang Huynh Thanh <quanght@tech.est-rouge.com>
2020-10-30 17:06:21 +0700

    Add get pending scripts

---
+++ vars/scriptApproval.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+import org.jenkinsci.plugins.scriptsecurity.scripts.*
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.*;
+
+// create new script approval object
+def call(){
+  def scriptApproval = ScriptApproval.get()
+  for(ScriptApproval.ApprovedWhitelist approved : scriptApproval.getApprovedSignatures()) {
+    println(approved.signature)
+  }
+
+  for(ScriptApproval pending: scriptApproval.getPendingSignatures()){
+    println(pending.signature)
+  }
+}
