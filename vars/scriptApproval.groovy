92bd7b1f8361b592f9594bca14ce4811f998c606
Quang Huynh Thanh <quanght@tech.est-rouge.com>
2020-11-02 16:46:06 +0700

    Integrate slack notification

--- vars/scriptApproval.groovy
+++ vars/scriptApproval.groovy
@@ -1,24 +1,74 @@
 #!/usr/bin/env groovy
 import org.jenkinsci.plugins.scriptsecurity.scripts.*
-import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.*;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.*
+import com.cloudbees.plugins.credentials.*
+import groovy.json.JsonOutput
 
-// create new script approval object
 def call(){
   def scriptApproval = ScriptApproval.get()
-  //for(ScriptApproval.ApprovedWhitelist approved : scriptApproval.getApprovedSignatures()) {
-  //  println(approved.signature)
-  //}
+  List<String> pendingScripts = [] as String[]
+  List<String> pendingSignatures = [] as String[]
+
   // Approve signature
   for(ScriptApproval pending: scriptApproval.getPendingSignatures()){
-    println("Pending signature: " + pending.signature)
-    println("Approving signature: " + pending.signature)
-    scriptApproval.approveSignature(pending.signature)
+    pendingSignatures.add(pending.signature)
+    // scriptApproval.approveSignature(pending.signature)
   }
 
   // Approve scripts
   for(ScriptApproval pending: scriptApproval.getPendingScripts()){
-    println("Pending script: " + pending.script)
-    println("Approving script: "+ pending.script)
-    scriptApproval.approveScript(pending.getHash())
+    pendingScripts.add(pending.script)
+    // scriptApproval.approveScript(pending.getHash())
+  }
+
+  send("",pendingScripts)
+  send("",pendingSignatures)
+}
+
+def send(String channel = "", List<String> pending = []) {
+  if(!pending.isEmpty()) {
+    def json = JsonOutput.toJson(
+      [
+        username: "Jenkins",
+        icon_url: "https://jenkins.io/images/logos/jenkins/jenkins.png",
+        attachments: [
+          [
+            color: "#764FA5",
+            blocks : [
+              [
+                type: "section",
+                text: [
+                  type: "mrkdwn",
+                  text: "Approved new pending scripts/signatures:\n\t```${pending.join('\n')}```"
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
+    ).toString()
+    println(json)
+
+    try {
+      def slack_url = ""
+      def credentials = CredentialsProvider.lookupCredentials(com.cloudbees.plugins.credentials.Credentials.class, Jenkins.instance)
+      for(cred in credentials) {
+        if(cred.id == "HOOK_URL") {
+          slack_url = cred.secret
+        }
+      }
+      def connection = new URL(slack_url.toString()).openConnection()
+      connection.setRequestMethod('POST')
+      connection.setRequestProperty('Content-Type', 'application/json')
+      connection.setDoOutput(true)
+      def output = new OutputStreamWriter(connection.outputStream)
+      output.write(json)
+      output.close()
+      if ( connection.responseCode != 200 ) {
+        error("Failed sending to slack")
+      }
+    } catch(err) {
+      println("${err.toString()}. Can't send message to slack.")
+    }
   }
 }
--- vars/slackSender.groovy
+++ vars/slackSender.groovy
@@ -1,11 +1,35 @@
-#!/usr/bin/env groovy
-import com.cloudbees.plugins.credentials.*
-
-def call() {
+def send(String channel = "", String pending = "") {
   def credentials = CredentialsProvider.lookupCredentials(com.cloudbees.plugins.credentials.Credentials.class, Jenkins.instance)
-
   for(cred in credentials) {
-    println(cred.id)
-    println(cred.secret)
+    if(cred.id == "HOOK_URL") {
+      slack_url = cred.secret
+    }
+  }
+
+  def json = JsonOutput.toJson(
+    [
+      "username": "Jenkins",
+      "icon_url": "https://jenkins.io/images/logos/jenkins/jenkins.png",
+      "attachments": [
+        [
+          "color": "#764FA5",
+          "blocks" : [
+            [
+              "type": "section",
+              "text": [
+                "type": "mrkdwn",
+                "text": "Found new pending approval scripts/signatures: ${pending}"
+              ]
+            ]
+          ]
+        ]
+      ]
+    ]
+  )
+
+  try {
+    sh "curl -X POST -H 'Content-type: application/json' --data '${json}' ${slack_url} --max-time 5"
+  } catch(err) {
+    println("${err} Couldn't send slack message. Moving on!")
   }
 }
